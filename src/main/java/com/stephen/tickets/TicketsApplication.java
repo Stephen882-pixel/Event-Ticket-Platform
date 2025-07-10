db889cefdad52a4429d5c67816bca6e06e68e8bd
Stephen882-pixel <ondeyostephen0@gmail.com>
2025-07-10 19:45:39 +0300

    configured map struct dependency

--- src/main/java/com/stephen/tickets/TicketsApplication.java
+++ src/main/java/com/stephen/tickets/TicketsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicketsApplication {
 
-	public static void main(String[] args)gi {
+	public static void main(String[] args) {
 		SpringApplication.run(TicketsApplication.class, args);
 	}
 
