ec87173be11958a3af6fa81f69b6004d01ba53bd
Stephen882-pixel <ondeyostephen0@gmail.com>
2025-07-10 16:22:49 +0300

    first commit

---
+++ src/main/java/com/stephen/tickets/TicketsApplication.java
@@ -0,0 +1,13 @@
+package com.stephen.tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsApplication {
+
+	public static void main(String[] args)gi {
+		SpringApplication.run(TicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stephen/tickets/TicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stephen.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
