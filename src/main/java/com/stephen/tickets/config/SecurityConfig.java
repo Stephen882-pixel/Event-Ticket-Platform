ed8451be52e7e50c1a5a4db56ff754688d4fba3b
Stephen882-pixel <ondeyostephen0@gmail.com>
2025-07-12 10:58:30 +0300

    Security config

---
+++ src/main/java/com/stephen/tickets/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.stephen.tickets.config;
+
+
+import com.stephen.tickets.filters.UserProvisioningFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(
+            HttpSecurity http,
+            UserProvisioningFilter userProvisioningFilter ) throws Exception {
+        http
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated())
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth ->
+                        oauth.jwt(
+                                Customizer.withDefaults()
+                        ))
+                .addFilterAfter(userProvisioningFilter, BearerTokenAuthenticationFilter.class);
+        return http.build();
+    }
+
+}
